599a60dc1c80081ebcaad6a20e0d566ef2d28db8
Sabbir Bin Abbas <sabbirba10@gmail.com>
2025-07-30 11:49:33 +0600

    remove unused icon in card-links component

--- src/components/card-links.tsx
+++ src/components/card-links.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { SiPaypal } from "@icons-pack/react-simple-icons";
 import { Heart } from "lucide-react";
 import Link from "next/link";
 import { ReactNode } from "react";
