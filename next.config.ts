16ee68d64b2f859e4e4664915a0af022ddf3379f
Sabbir Bin Abbas <sabbirba10@gmail.com>
2025-07-30 13:20:53 +0600

    Refactor next.config.ts to use ES module syntax for environment variables

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  swcMinify: true,
+import type { NextConfig } from "next";
+module.exports = {
   env: {
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   },
+}
+const nextConfig = {
+  env: {
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+  },
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.githubusercontent.com" },
