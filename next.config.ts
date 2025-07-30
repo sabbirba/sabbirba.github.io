e2e86b5ec6755d25454bd41c55bc8721407faa3f
Sabbir Bin Abbas <sabbirba10@gmail.com>
2025-07-30 11:55:19 +0600

    add environment variables for Upstash Redis configuration

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
-
+module.exports = {
+  env: {
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+  },
+}
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
